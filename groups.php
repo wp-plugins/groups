9cc05917fcd018a613469ee472d112677a1032ad
itthinx <itthinx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-17 21:29:04 +0000

    beta 3b
    git-svn-id: https://plugins.svn.wordpress.org/groups/trunk@520283 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- groups.php
+++ groups.php
@@ -21,7 +21,7 @@
  * Plugin Name: Groups
  * Plugin URI: http://www.itthinx.com/plugins/groups
  * Description: Groups provides group-based user membership management, group-based capabilities and content access control.
- * Version: 1.0.0-beta-3
+ * Version: 1.0.0-beta-3b
  * Author: itthinx (Karim Rahimpur)
  * Author URI: http://www.itthinx.com
  * Donate-Link: http://www.itthinx.com
