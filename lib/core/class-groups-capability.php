af8bccfd3e1f31339aa4fcbec5592cfe2690ab1a
itthinx <itthinx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-06 21:15:16 +0000

    1.4.14 changeset with Swedish translation added and performance improvements
    git-svn-id: https://plugins.svn.wordpress.org/groups/trunk@1129054 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/core/class-groups-capability.php
+++ lib/core/class-groups-capability.php
@@ -27,7 +27,10 @@ if ( !defined( 'ABSPATH' ) ) {
  * Capability OPM
  */
 class Groups_Capability {
-	
+
+	const CACHE_GROUP        = 'groups';
+	const READ_BY_CAPABILITY = 'read_by_capability';
+
 	/**
 	 * @var persisted capability object
 	 */
@@ -152,6 +155,8 @@ class Groups_Capability {
 				$capability_table = _groups_get_tablename( 'capability' );
 				if ( $wpdb->insert( $capability_table, $data, $formats ) ) {
 					if ( $result = $wpdb->get_var( "SELECT LAST_INSERT_ID()" ) ) {
+						// read_by_capability above created a cache entry which needs to be reset
+						wp_cache_delete( self::READ_BY_CAPABILITY . '_' . $capability, self::CACHE_GROUP );
 						do_action( "groups_created_capability", $result );
 					}
 				}
@@ -191,20 +196,23 @@ class Groups_Capability {
 	 */
 	public static function read_by_capability( $capability ) {
 		global $wpdb;
-		$result = false;
-		
-		$capability_table = _groups_get_tablename( 'capability' );
-		$capability = $wpdb->get_row( $wpdb->prepare(
-			"SELECT * FROM $capability_table WHERE capability = %s",
-			$capability
-		) );
-		if ( isset( $capability->capability_id ) ) {
-			$result = $capability;
+		$_capability = $capability;
+		$result = wp_cache_get( self::READ_BY_CAPABILITY . '_' . $_capability, self::CACHE_GROUP, false, $found );
+		if ( $found === false ) {
+			$result = false;
+			$capability_table = _groups_get_tablename( 'capability' );
+			$capability = $wpdb->get_row( $wpdb->prepare(
+				"SELECT * FROM $capability_table WHERE capability = %s",
+				$capability
+			) );
+			if ( isset( $capability->capability_id ) ) {
+				$result = $capability;
+			}
+			wp_cache_set( self::READ_BY_CAPABILITY . '_' . $_capability, $result, self::CACHE_GROUP );
 		}
 		return $result;
 	}
-	
-	
+
 	/**
 	 * Update capability.
 	 * 
@@ -231,6 +239,7 @@ class Groups_Capability {
 					$old_capability->object = $object;
 				}
 				if ( isset( $name ) ) {
+					$old_name = $old_capability->name;
 					$old_capability->name = $name;
 				}
 				if ( isset( $description ) ) {
@@ -247,6 +256,12 @@ class Groups_Capability {
 				) );
 				if ( ( $rows !== false ) ) {
 					$result = $capability_id;
+					if ( !empty( $old_capability ) && !empty( $old_capability->name ) ) {
+						wp_cache_delete( self::READ_BY_CAPABILITY . '_' . $old_capability->name, self::CACHE_GROUP );
+					}
+					if ( !empty( $old_name ) ) {
+						wp_cache_delete( self::READ_BY_CAPABILITY . '_' . $old_name, self::CACHE_GROUP );
+					}
 					do_action( "groups_updated_capability", $result );
 				}
 			}
@@ -274,6 +289,9 @@ class Groups_Capability {
 				Groups_Utility::id( $capability_id )
 			) ) ) {
 				$result = $capability_id;
+				if ( !empty( $capability->name ) ) {
+					wp_cache_delete( self::READ_BY_CAPABILITY . '_' . $capability->name, self::CACHE_GROUP );
+				}
 				do_action( "groups_deleted_capability", $result );
 			}
 		}
